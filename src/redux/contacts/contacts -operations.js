6bd3f8a4c58e0726a3ea6ff991a5d3d011f8240f
ViktoriaFomenko <aktavia@gmail.com>
2022-09-10 21:24:59 +0300

    final before verification

--- src/redux/contacts/contacts -operations.js
+++ src/redux/contacts/contacts -operations.js
@@ -1,40 +1,38 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
 import * as api from 'shared/api/contacts';
-import actions from './contacts -actions';
 
-export const fetchContacts = () => {
-  const func = async dispatch => {
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetch',
+  async (_, thunkAPI) => {
     try {
-      dispatch(actions.fetchContactsLoading());
       const data = await api.getContacts();
-      dispatch(actions.fetchContactsSuccess(data));
+      return data;
     } catch (error) {
-      dispatch(actions.fetchContactsError(error.message));
+      return thunkAPI.rejectWithValue(error);
     }
-  };
-  return func;
-};
-export const addContact = data => {
-  const func = async dispatch => {
-    try {
-      dispatch(actions.addContactLoading());
-      const result = await api.addContact(data);
-      dispatch(actions.addContactSuccess(result));
-    } catch (error) {
-      dispatch(actions.addContactError(error.message));
-    }
-  };
-  return func;
-};
+  }
+);
 
-export const removeContact = id => {
-  const func = async dispatch => {
+export const addContact = createAsyncThunk(
+  'contacts/add',
+  async (data, { rejectWithValue }) => {
     try {
-      dispatch(actions.removeContactLoading());
-      await api.removeContact(id);
-      dispatch(actions.removeContactSuccess(id));
+      const result = await api.addContact(data);
+      return result;
     } catch (error) {
-      dispatch(actions.removeContactError(error.message));
+      return rejectWithValue(error);
     }
-  };
-  return func;
-};
+  }
+);
+
+export const removeContact = createAsyncThunk(
+  'contacts/remove',
+  async (id, { rejectWithValue }) => {
+    try {
+      await api.removeContact(id);
+      return id;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import { persistStore, persistReducer } from 'redux-persist';
-import contactsReducer from './contacts/contacts -reducer';
+import contactsReducer from './contacts/contacts -slice';
 import filterReducer from './filter/filter -reducer';
-// import storage from 'redux-persist/lib/storage';
-// import {
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
 
 const store = configureStore({
   reducer: {
@@ -20,25 +10,3 @@ const store = configureStore({
 });
 
 export default store;
-
-// const contactsPersistConfig = {
-//   key: 'contacts',
-//   storage,
-//   whitelist: ['contacts'],
-// };
-// const persistedContactsReducer = persistReducer(
-//   contactsPersistConfig,
-//   rootReducer
-// );
-
-// export const store = configureStore({
-//   reducer: persistedContactsReducer,
-//   middleware: getDefaultMiddleware =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-// });
-
-// export const persistore = persistStore(store);
